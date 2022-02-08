42270281e864cf5abb68669468b8efb77deb6cac
annngure <annwanjikungure@gmail.com>
2022-02-08 22:26:29 +0300

    add views.py

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordFeild,SubmitField
 from wtforms.validators import Required,Email,EqualTo
+from wtforms import ValidationError
 from ..models import User
 
 class RegistrationForm(FlaskForm):
@@ -8,4 +9,13 @@ class RegistrationForm(FlaskForm):
     username = StringField('Enter your username',validators=[Required()])
     password = PasswordFeild('Password',validators = [Required(), EqualTo('password_confirm', message = 'Password must match')])
     password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
-    submit = SubmitField('Sign Up') 
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError('There is an account with that email')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data.feild.data).first():
+            raise ValidationError('That username is taken')
+
