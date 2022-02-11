6d795d6abd31995f499f96cc8bfa4f0d1ca5b387
annngure <annwanjikungure@gmail.com>
2022-02-11 22:47:15 +0300

    intialize mail

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 from flask_login import LoginManager
+from flask_mail import Mail
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view ='auth.login'
-
+mail=Mail()
 
 def create_app(config_name):
 
@@ -14,3 +15,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+
+def create_app(config_name):
+    app = Flask(__name__)
+    mail.init_app(app)
